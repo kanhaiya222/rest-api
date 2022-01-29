0c72515e1776c6a94932ad84a5c85ccff15bcdb9
Kanhaiya <kanhaiyayadav222@gmail.com>
2022-01-29 19:07:53 +0530

    worked with jenkins

--- src/main/java/com/restapi/restapi/RestapiApplication.java
+++ src/main/java/com/restapi/restapi/RestapiApplication.java
@@ -2,12 +2,21 @@ package com.restapi.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestapiApplication {
+public class RestapiApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestapiApplication.class, args);
 	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	return builder.sources(RestapiApplication.class);
+	}
+
+
 
 }
